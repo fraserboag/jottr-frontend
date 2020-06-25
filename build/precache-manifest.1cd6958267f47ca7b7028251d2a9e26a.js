fc2ff01e6db329671a2a5ba20a7e268bb6312333
Fraser Boag <fraser.boag@gmail.com>
2020-06-25 22:56:22 +0100

    building locally as server doesn't have enough memory

---
+++ build/precache-manifest.1cd6958267f47ca7b7028251d2a9e26a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e704280877be6548fb3b2e199bc14bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c2b61839911bc4fd72a",
+    "url": "/static/css/main.b5aac506.chunk.css"
+  },
+  {
+    "revision": "593e2bc42c34b1f08ed5",
+    "url": "/static/js/2.5fa9c954.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5fa9c954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2b61839911bc4fd72a",
+    "url": "/static/js/main.87bd477c.chunk.js"
+  },
+  {
+    "revision": "654eb14992b1b89ff2bf",
+    "url": "/static/js/runtime-main.de104e39.js"
+  }
+]);
